1ea937e8d301f2157f1254d31ad9e3112f35c9c6
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-02-05 17:23:20 -0800

    Update index.js
    added the forgotten semicolo :smile:

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,1 @@
-alert ('Welcome to my Portfolio.\nIt would be fully functional by 01/03/2021.\nHave a great day ahead!Bye.')
+alert ('Welcome to my Portfolio.\nIt would be fully functional by 01/03/2021.\nHave a great day ahead!Bye.');
